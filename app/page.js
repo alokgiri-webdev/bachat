451d2eda6c6e6e24506d03f003e476f6487cb909
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-05-06 00:19:10 +0530

    Setup prisma DB, Arcjet Security & Inggest for handling backend AI functions & Create HeroSection

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import {Button} from '@/components/ui/button';
+import HeroSection from '@/components/herosection';
+
 export default function Home() {
   return (
     <div className="mt-40">
-<Button variant='destructive'> Welcome to Bachat </Button>
-    </div>
-    
+    <HeroSection/>
+    </div>  
   );
 }
