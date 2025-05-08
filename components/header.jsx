6ec7c94738a71741afaf4dee6e15ebec873669d6
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-05-08 11:39:19 +0530

    Debugged Prisma Client issue

--- components/header.jsx
+++ components/header.jsx
@@ -5,8 +5,10 @@ import Image from "next/image";
 import {Button} from '@/components/ui/button';
 import {LayoutDashboard} from 'lucide-react';
 import {PenBox} from 'lucide-react';
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+const Header = async () => {
+  await checkUser();
   return (
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
