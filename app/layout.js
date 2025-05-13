2458e2bdb097b646e2af77bd8ae4d7db88f085a7
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-05-13 15:49:31 +0530

    Create fully functional Dashboard

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,35 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
-import {ClerkProvider} from "@clerk/nextjs"
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
-const inter = Inter({subsets:["latin"]});
+const inter = Inter({ subsets: ["latin"] });
+
 export const metadata = {
-  title: "Bachat",
-  description: "Your Personalized Budget Management Platform",
+  title: "Welth",
+  description: "One stop Finance Platform",
 };
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${inter.className}`}
-      >
-        {/*header*/}
-        <Header/>
-        <main className="min-h-screen">{children}</main>
-        {/*footer*/}
-        <footer className="bg-blue-50 py-12">
-        <div className="container mx-auto px-4 text-center text-gray-600">
-          <p>Planning Today for a Wealthier Tomorrow</p>
-        </div>
-        </footer>
-      </body>
-    </html>
+      <html lang="en">
+        <head>
+          <link rel="icon" href="/logo-sm.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
+
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made with 💗 by RoadsideCoder</p>
+            </div>
+          </footer>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
